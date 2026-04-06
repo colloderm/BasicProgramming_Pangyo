a4d36344e1eba84e7edce213ff7b686c4e0468f4
colloderm <colloderm@gmail.com>
2026-04-06 18:26:47 +0900

    C++ 1일차 기초공부

--- L01StandardIO/L01StandardIO.cpp
+++ L01StandardIO/L01StandardIO.cpp
@@ -1,3 +1,6 @@
+// 전처리기
+#include <cstdio>
+
 /**
  * < main 함수란? >
  * 모든 C/C++로 개발된 프로그램(exe)은 한 개 이상의 함수로 구성.
@@ -5,8 +8,22 @@
  * 이중 반드시 있어야하고, 단 하나만 존재하는 함수가 이 main 함수
  * 당연히 없으면 컴파일 자체가 안됨.
  */
-int main(int argc, char* argv[])
+int main()
 {
+    // 정수형 변수 선언
+    int Num1 = 0;
+    int Num2 = 0;
+    
+    scanf_s("%d", &Num1);
+    scanf_s("%d", &Num2);
+    
+    printf("Num1: %d, ", Num1);
+    printf("Num2: %d\n", Num2);
+    printf("Num1 + Num2 = %d\n", Num1 + Num2);
+    printf("Num1 - Num2 = %d\n", Num1 - Num2);
+    printf("Num1 * Num2 = %d\n", Num1 * Num2);
+    printf("Num1 / Num2 = %d\n", Num1 / Num2);
+    printf("Num1 %% Num2 = %d\n", Num1 % Num2);
     
     return 0;
 }
